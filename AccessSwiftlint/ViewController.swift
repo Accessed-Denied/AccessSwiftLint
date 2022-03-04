6d8dfcc4b0aa026d288948f9e4ebfa2642440c2c
rohitsainier <rohitsainier@gmail.com>
2022-03-04 23:04:32 +0530

    code updated

---
+++ AccessSwiftlint/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AccessSwiftlint
+//
+//  Created by Rohit Saini on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
